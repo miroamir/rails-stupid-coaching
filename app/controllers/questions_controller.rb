67872abe44a28d6524be5522512697b1d5f46a54
119443772+miroamir@users.noreply.github.com <miroamir422@gmail.com>
2023-02-20 16:48:42 +0000

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+   @question = params[:question]
+
+  if @question == "I am going to work"
+    @reply = "great"
+  elsif @question.include?("?")
+
+    @reply = "Silly question, get dressed and go to work!"
+  else
+    @reply = "I don't care, get dressed and go to work!"
+  end
+
+end
+end
